3c71f33936bc3d929810038ff9f0dbee7ee92d00
CrisDang3r <105089039+CrisDang3r@users.noreply.github.com>
2023-03-13 03:20:42 -0300

    cenario e cenario outline funcionando

--- features/step-definitions/calcular.js
+++ features/step-definitions/calcular.js
@@ -1,37 +1,36 @@
-const { Given, When, Then, After } = require("@wdio/cucumber-framework");
+const { Before, AfterAll, Given, When, Then } = require("@wdio/cucumber-framework");
 
-After(async () =>{
-    await driver.closeApp()
-})
+let display;
+
+
 
 Given(/^que a calculadora está aberta$/, async () => {
-
-    const display = '//android.widget.TextView[@content-desc="No result"]'
-
+    // inicia a calculadora
+    await driver.launchApp();
+    display = '//android.widget.TextView[@content-desc="No result"]';
+    
     // validar se carregou o display de resultado
-    await $(display).waitForDisplayed()
-    // validar se o display está exibindo o número 0
-    //expect(await $(display).getText().toEqual("0"))
-})
+    await $(display).waitForDisplayed();
+});
 
 When(/^clico no botao "([^"]*)?"$/, async (numero) => {
-    const buttonNumber = `//android.widget.ImageButton[@content-desc="${numero}"]`
-    await $(buttonNumber).click()
-})
+  const buttonNumber = `//android.widget.ImageButton[@content-desc="${numero}"]`;
+  await $(buttonNumber).click();
+});
 
 When(/^clico no botao Somar$/, async () => {
-    const buttonPlus = `//android.widget.ImageButton[@content-desc="plus"]`
-    await $(buttonPlus).click()
-})
+  const buttonPlus = `//android.widget.ImageButton[@content-desc="plus"]`;
+  await $(buttonPlus).click();
+});
 
 When(/^clico no botao igual$/, async () => {
-    const buttonEqual = `//android.widget.ImageButton[@content-desc="equals"]`
-    await $(buttonEqual).click()
-})
+  const buttonEqual = `//android.widget.ImageButton[@content-desc="equals"]`;
+  await $(buttonEqual).click();
+});
 
 Then(/^exibe o resultado como "([^"]*)?"$/, async (numero) => {
-    const display = "/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.LinearLayout/android.widget.FrameLayout[1]/android.widget.TextView"
+  const displayResult = "/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.LinearLayout/android.widget.FrameLayout[1]/android.widget.TextView";
 
-    // validar o resultado esperado
-    expect(await $(display).getText()).toEqual(numero)
-})
+  // validar o resultado esperado
+  expect(await $(displayResult).getText()).toEqual(numero);
+});
